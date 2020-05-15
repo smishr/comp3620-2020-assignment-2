b9cdd1a9ebe4aad4a7d4646351db83e5d575ab18
Zheyuan Zhang <u6870923@anu.edu.au>
2020-05-15 11:06:45 +1000

    finish b part q6 Zheyuan Zhang 2020.5.14

--- wumpus2csp.py
+++ wumpus2csp.py
@@ -76,39 +76,7 @@ def main():
     for obs in observations:
         observed_loc.append(obs['location'])
 
-    # Create possible wumpuses location
-    wumpuse_loc = []
-    for obs in observations:
-        if 'Stench' in obs['percepts']:
-            new_loc = [[obs['location'][0], obs['location'][1] + 1], [obs['location'][0], obs['location'][1] - 1],
-                       [obs['location'][0] + 1, obs['location'][1]], [obs['location'][0] - 1, obs['location'][1]]]
-            for loc in new_loc:
-                if loc[0] < 1 or loc[0] > n_columns or loc[1] < 1 or loc[1] > n_rows:
-                    continue
-                elif loc in observed_loc:
-                    continue
-                else:
-                    if loc not in wumpuse_loc:
-                        wumpuse_loc.append(loc)
-    # print(wumpuse_loc)
-
-    # Create possible pits location
-    pit_loc = []
-    for obs in observations:
-        if 'Breeze' in obs['percepts']:
-            new_loc = [[obs['location'][0], obs['location'][1] + 1], [obs['location'][0], obs['location'][1] - 1],
-                       [obs['location'][0] + 1, obs['location'][1]], [obs['location'][0] - 1, obs['location'][1]]]
-            for loc in new_loc:
-                if loc[0] < 1 or loc[0] > n_columns or loc[1] < 1 or loc[1] > n_rows:
-                    continue
-                elif loc in observed_loc:
-                    continue
-                else:
-                    if loc not in pit_loc:
-                        pit_loc.append(loc)
-    # print(pit_loc)
-
-    # Create possible moves
+    # move according to action
     current_loc = [observations[-1]['location'][0], observations[-1]['location'][1]]
     if action == 'north':
         moves = [[current_loc[0], current_loc[1] + 1]]
@@ -119,35 +87,138 @@ def main():
     else:
         moves = [[current_loc[0] - 1, current_loc[1]]]
 
-    # moves = []
-    # possible_moves = [[current_loc[0], current_loc[1] + 1], [current_loc[0], current_loc[1] - 1],
-    #                   [current_loc[0] + 1, current_loc[1]], [current_loc[0] - 1, current_loc[1]]]
-    # for move in possible_moves:
-    #     if move[0] < 1 or move[0] > n_columns or move[1] < 1 or move[1] > n_rows:
-    #         continue
-    #     elif move in observed_loc:
-    #         continue
-    #     else:
-    #         if move not in moves:
-    #             moves.append(move)
-    # print(moves)
-
     # Create variables map
     variables = {'a': moves}
-    for w in range(n_wumpuses):
-        variables['w' + str(w)] = wumpuse_loc
-    for p in range(n_pits):
-        variables['p' + str(p)] = pit_loc
+    wumpus_variable = {}
+    pit_variable = {}
+
+    # Create possible wumpuses location
+    count = 0
+    for obs in observations:
+        if 'Stench' in obs['percepts']:
+            new_w = 'w' + str(count)
+            wumpus_variable[new_w] = []
+            new_loc = [[obs['location'][0], obs['location'][1] + 1], [obs['location'][0], obs['location'][1] - 1],
+                       [obs['location'][0] + 1, obs['location'][1]], [obs['location'][0] - 1, obs['location'][1]]]
+            for loc in new_loc:
+                if loc[0] < 1 or loc[0] > n_columns or loc[1] < 1 or loc[1] > n_rows:
+                    continue
+                elif loc in observed_loc:
+                    continue
+                else:
+                    if loc not in wumpus_variable[new_w]:
+                        wumpus_variable[new_w].append(loc)
+            count += 1
+
+    # Create possible pits location
+    count = 0
+    for obs in observations:
+        if 'Breeze' in obs['percepts']:
+            new_p = 'p' + str(count)
+            pit_variable[new_p] = []
+            new_loc = [[obs['location'][0], obs['location'][1] + 1], [obs['location'][0], obs['location'][1] - 1],
+                       [obs['location'][0] + 1, obs['location'][1]], [obs['location'][0] - 1, obs['location'][1]]]
+            for loc in new_loc:
+                if loc[0] < 1 or loc[0] > n_columns or loc[1] < 1 or loc[1] > n_rows:
+                    continue
+                elif loc in observed_loc:
+                    continue
+                else:
+                    if loc not in pit_variable[new_p]:
+                        pit_variable[new_p].append(loc)
+            count += 1
+
+    special_con_list = {}
+    import itertools
+    wumpus_special = False
+    pit_special = False
+    # create special constrain for wumpuses
+    if len(wumpus_variable.keys()) > n_wumpuses:
+        wumpus_special = True
+        # get all the permutation
+        possible_list = list(itertools.product(*wumpus_variable.values()))
+        # print(possible_list)
+        key_set = 'con '
+        for key in wumpus_variable.keys():
+            key_set += key + ' '
+        temp_con = []
+        if n_wumpuses == 1:
+            # all the elements must be same
+            for candidate in possible_list:
+                if candidate[1:] == candidate[:-1]:
+                    if key_set not in special_con_list.keys():
+                        temp_con.append(candidate)
+                        # print(special_con_list)
+            special_con_list[key_set] = temp_con
+        else:
+            for p in possible_list:
+                for val in p:
+                    if p.count(val) == n_wumpuses and p not in temp_con:
+                        temp_con.append(p)
+                        # print(p)
+            special_con_list[key_set] = temp_con
+        # print(special_con_list)
+    else:
+        for key in wumpus_variable.keys():
+            variables[key] = wumpus_variable[key]
+
+    # create special constrain for pits
+    if len(pit_variable.keys()) > n_pits:
+        pit_special = True
+        # get all the permutation
+        possible_list = list(itertools.product(*pit_variable.values()))
+        # print(possible_list)
+        key_set = 'con '
+        for key in pit_variable.keys():
+            key_set += key + ' '
+        temp_con = []
+        if n_pits == 1:
+            # all the elements must be same
+            for candidate in possible_list:
+                if candidate[1:] == candidate[:-1]:
+                    if key_set not in special_con_list.keys():
+                        temp_con.append(candidate)
+                        # print(special_con_list)
+            special_con_list[key_set] = temp_con
+        else:
+            for p in possible_list:
+                for val in p:
+                    if p.count(val) == n_pits and p not in temp_con:
+                        temp_con.append(p)
+                        # print(p)
+            special_con_list[key_set] = temp_con
+        # print(special_con_list)
+    else:
+        for key in pit_variable.keys():
+            variables[key] = pit_variable[key]
+
     # print(variables)
 
-    # Create constrain map:
+    # Create normal constrain
     con_list = {}
     for key1 in variables.keys():
         for key2 in variables.keys():
             if key2 != key1 and ((key2, key1) not in con_list.keys() and (key1, key2) not in con_list.keys()):
                 con_list[(key1, key2)] = ''
     # print(con_list)
+    if wumpus_special:
+        for w in wumpus_variable.keys():
+            con_list[('a', w)] = ''
+            for p in pit_variable.keys():
+                con_list[(w, p)] = ''
 
+    if pit_special:
+        for p in pit_variable.keys():
+            con_list[('a', p)] = ''
+            for w in wumpus_variable.keys():
+                con_list[(p, w)] = ''
+
+    for key in wumpus_variable:
+        variables[key] = wumpus_variable[key]
+    for key in pit_variable:
+        variables[key] = pit_variable[key]
+
+    # print(con_list)
     for key in con_list.keys():
         for val1 in variables[key[0]]:
             for val2 in variables[key[1]]:
@@ -162,7 +233,7 @@ def main():
     try:
         scenario = (args.input.split('/')[1]).split('.')[0]
         target = 'b'
-        new_out = output_path+'/'+scenario+'_'+action+'_'+target+'.csp'
+        new_out = output_path + '/' + scenario + '_' + action + '_' + target + '.csp'
         path = os.path.exists(output_path)
         if not path:
             os.makedirs(output_path)
@@ -176,8 +247,21 @@ def main():
         for con, values in con_list.items():
             if values:
                 file.write('con ' + con[0] + ' ' + con[1] + ' : ' + values[:-3] + '\n')
+        for con, values in special_con_list.items():
+            if values:
+                val_str = ''
+                for v in values:
+                    if len(v) == 2:
+                        val_str += '<' + str(v[0][0]) + ',' + str(v[0][1]) + '>' + ' ' + '<' + str(v[1][0]) + ',' + str(
+                            v[1][1]) + '> : '
+                    else:
+                        val_str += '<' + str(v[0][0]) + ',' + str(v[0][1]) + '>' + ' ' + '<' + str(v[1][0]) + ',' + str(
+                            v[1][1]) + '>' + ' ' + '<' + str(v[2][0]) + ',' + str(v[2][1]) + '> : '
+            file.write(con + ': ' + val_str[:-3] + '\n')
 
         file.close()
+        from reference_n_to_bin import convert
+        convert(new_out, new_out)
 
     except IOError:
         print("failed")
@@ -185,3 +269,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
